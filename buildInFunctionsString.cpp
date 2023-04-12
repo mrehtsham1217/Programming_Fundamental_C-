b367956c8ff0fbf6fa01c845ec098baf30a90507
MR_EHTSHAM <125168180+mrehtsham1217@users.noreply.github.com>
2023-04-12 08:39:45 +0500

    Add files via upload

---
+++ buildInFunctionsString.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+/*build in functions in string
+01---size(),length()
+02---clear()
+03 ---front(),end()
+04---append("",3)
+05---push_back(),pop_back()
+06---swap()
+07---erase(str.begin() + 1)
+08---find(),rfind()
+09---replace()
+10--SUBSTRING()
+*/
+}
